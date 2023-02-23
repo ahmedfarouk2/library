33874b41d70763d840d774f77ae52a6f0cb8b17d
ahmedfarouk2 <ahmedoza12@gmail.com>
2023-02-23 05:24:19 +0200

    Made form pop up when adding book and closing form when pressing close button

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const addButton = document.querySelector('.add-button');
+const closeFormButton = document.querySelector('.close-button');
+const inputForm = document.querySelector('.input-parent');
 
+function addButtonClick() {
+  inputForm.classList.remove('none');
+}
+
+function closeFormClick() {
+    inputForm.classList.add('none');
+}
+
+addButton.addEventListener('click', addButtonClick);
+closeFormButton.addEventListener('click', closeFormClick);
