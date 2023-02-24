7774ad8cfbda683a6759375971816d6a6637ff8a
ahmedfarouk2 <ahmedoza12@gmail.com>
2023-02-24 05:36:32 +0200

    Logic added for adding a book and changing it's read status

--- app.js
+++ app.js
@@ -1,14 +1,68 @@
+const bookLibrary = [];
 const addButton = document.querySelector('.add-button');
 const closeFormButton = document.querySelector('.close-button');
 const inputForm = document.querySelector('.input-parent');
+const library = document.querySelector('.library');
+const book = document.querySelector('.book');
+const bookName = document.querySelector('#book-name');
+const bookAuthor = document.querySelector('#book-author');
+const bookRead = document.querySelector('#read');
+const submitButton = document.querySelector('.submit-button');
+
+class Book {
+  constructor(name, author, read) {
+    this.name = name;
+    this.author = author;
+    this.read = read;
+  }
+}
+
+function bookAppending() {
+  const newBook = document.createElement('div');
+  newBook.classList.add('book');
+  newBook.append(`Book name: ${bookLibrary[bookLibrary.length - 1].name}`);
+  newBook.innerHTML += '<br>';
+  newBook.append(`Book author: ${bookLibrary[bookLibrary.length - 1].author}`);
+  newBook.innerHTML += '<br>';
+  newBook.append('Have I read this book?:');
+  newBook.innerHTML += `<span class="read-span">${bookLibrary[bookLibrary.length - 1].read}</span>`;
+  if (bookLibrary[bookLibrary.length - 1].read === true) {
+    library.append(newBook);
+  } else {
+    newBook.innerHTML += '<span class="question-span">Did you finish reading this book? <input type="checkbox" name="finish-reading" id="finish-reading"></span>';
+    library.append(newBook);
+    const finishedReading = document.querySelectorAll('#finish-reading');
+    const questionSpan = document.querySelectorAll('.question-span');
+    for (let i = 0; i < finishedReading.length; i++) {
+      finishedReading[i].addEventListener('click', () => {
+        const readSpan = document.querySelectorAll('.read-span');
+        readSpan[i].innerText = 'true';
+        finishedReading[i].style.display = 'none';
+        questionSpan[i].style.display = 'none';
+      });
+    }
+  }
+}
 
 function addButtonClick() {
   inputForm.classList.remove('none');
+  library.style.display = 'none';
 }
 
 function closeFormClick() {
-    inputForm.classList.add('none');
+  inputForm.classList.add('none');
+  library.style.display = 'grid';
 }
 
+function submitFormClick(e) {
+  e.preventDefault();
+  const bookToAdd = new Book(bookName.value, bookAuthor.value, bookRead.checked);
+  bookLibrary.push(bookToAdd);
+  inputForm.classList.add('none');
+  library.style.display = 'grid';
+  bookAppending();
+}
+
+submitButton.addEventListener('click', submitFormClick);
 addButton.addEventListener('click', addButtonClick);
 closeFormButton.addEventListener('click', closeFormClick);
